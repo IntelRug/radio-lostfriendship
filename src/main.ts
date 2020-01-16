3053407c7624b70073502ed9588a6fec71af6bcb
Svyatoslav Fyodorov <intelrug@gmail.com>
2020-01-17 01:02:04 +0400

    Add svg support for ie 11

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
+import svg4everybody from 'svg4everybody';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+svg4everybody();
 
 Vue.config.productionTip = false;
 
