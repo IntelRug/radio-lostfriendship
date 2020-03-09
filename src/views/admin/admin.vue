0d0654fcb957d1b96c1fb2514d95413f1d540d67
Svyatoslav Fyodorov <intelrug@gmail.com>
2020-03-10 01:32:27 +0400

    Add meta info

---
+++ src/views/admin/admin.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  include ../../tools/mixins
+  +content
+    +e.map
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import '@/components/content/content.styl';
+
+@Component({})
+export default class Home extends Vue {}
+</script>
+
+<style lang="stylus" src="./home.styl" />
