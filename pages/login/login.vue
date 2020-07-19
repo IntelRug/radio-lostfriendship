18741f576ab3be560d273d5ec273b5b4b16095c9
intelrug <intelrug@gmail.com>
2020-07-19 22:20:45 +0400

    feat: implement login page functionality

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -2,15 +2,23 @@
   <!-- begin .login-->
   <div class="login">
     <div class="login__form">
-      <b-form title="Вам сюда нельзя!">
-        <b-text-field id="login_email" placeholder="E-mail" width-full />
+      <b-form title="Вам сюда нельзя!" :errors="errors">
+        <b-text-field
+          id="login_email"
+          v-model="email"
+          placeholder="E-mail"
+          width-full
+        />
         <b-text-field
           id="login_password"
+          ref="password"
+          v-model="password"
           placeholder="Пароль"
           type="password"
           width-full
+          @keydown.enter="next()"
         />
-        <b-button class="login__button" width-full>
+        <b-button class="login__button" width-full @click="next()">
           Войти
         </b-button>
       </b-form>
@@ -20,16 +28,71 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator';
+import { Component, Vue, Mutation, Ref } from 'nuxt-property-decorator';
+import { MutationMethod } from 'vuex';
 import BForm from '~/components/form/form.vue';
 import BTextField from '~/components/text-field/text-field.vue';
 import BButton from '~/components/button/button.vue';
+import { MutationSignInArgs, SignInMutation } from '~/graphql/schema';
+import SignIn from '~/graphql/mutations/login.graphql';
+import { ExecutionResult } from '~/types/apollo';
 
 @Component({
   name: 'pg-login',
   components: { BButton, BTextField, BForm },
 })
-export default class Login extends Vue {}
+export default class Login extends Vue {
+  @Mutation setMyId!: MutationMethod;
+  @Mutation login!: MutationMethod;
+
+  @Ref('password') passwordField!: BTextField;
+
+  errors: string[] = [];
+
+  email: string = '';
+  password: string = '';
+
+  clearErrors() {
+    this.errors = [];
+  }
+
+  validateForm(): boolean {
+    this.clearErrors();
+    return true;
+  }
+
+  async next() {
+    if (!this.validateForm()) return;
+    await this.requestSignIn();
+  }
+
+  async requestSignIn() {
+    const variables: MutationSignInArgs = {
+      email: this.email,
+      password: this.password,
+    };
+
+    try {
+      const {
+        data,
+        errors,
+      }: ExecutionResult<SignInMutation> = await this.$apollo.mutate({
+        mutation: SignIn,
+        variables,
+      });
+
+      if (errors) this.errors = [errors.message];
+      if (!data) return;
+
+      await this.$apolloHelpers.onLogin(data.signIn.value, undefined, {
+        maxAge: 86400 * 365 * 10,
+      });
+      this.setMyId(data.signIn.ownerId);
+      this.login();
+      await this.$router.push('/');
+    } catch (e) {}
+  }
+}
 </script>
 
 <style lang="stylus" src="./login.styl" />
