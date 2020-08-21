207206ef56b1dab87f3887ee35b724969851afc6
intelrug <intelrug@gmail.com>
2020-08-22 03:05:59 +0400

    feat: create buttons and link under player, create announcements block

---
+++ components/player-buttons/player-buttons.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- begin .player-buttons-->
+  <div class="player-buttons">
+    <div class="player-buttons__buttons-container">
+      <router-link
+        v-for="(button, i) in buttons"
+        :key="i"
+        class="player-buttons__button"
+        :to="button.href"
+      >
+        {{ button.title }}
+      </router-link>
+    </div>
+    <div class="player-buttons__links-container">
+      <a
+        v-for="(link, i) in links"
+        :key="i"
+        :href="link.href"
+        class="player-buttons__link"
+        :title="link.title"
+      >
+        <svg-icon :name="link.icon" />
+      </a>
+    </div>
+  </div>
+  <!-- end .player-buttons-->
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component({
+  name: 'b-player-buttons',
+})
+export default class PlayerButtons extends Vue {
+  buttons = [
+    { title: 'История', href: '#' },
+    { title: 'Открыть чат', href: '#' },
+    { title: 'Записи', href: '#' },
+  ];
+  links = [
+    { title: 'Discord', icon: 'discord', href: '#' },
+    { title: 'VK', icon: 'vk', href: '#' },
+    { title: 'YouTube', icon: 'youtube', href: '#' },
+  ];
+}
+</script>
+
+<style lang="stylus" src="./player-buttons.styl" />
--- pages/main/main.vue
+++ pages/main/main.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="main">
     <div class="main__player">
-      <b-tile class="tile_padding_medium tile_borders_top">
+      <b-tile class="tile_padding_small tile_borders_top main__row">
+        <a href="#" class="main__announcement">
+          <span>27.08.2020</span> в <span>20:30</span> <sup>UTC+3</sup> –
+          Гостевая рубрика с Midipon
+        </a>
+      </b-tile>
+      <b-tile class="tile_padding_medium main__row">
         <b-player />
+      </b-tile>
+      <b-tile class="tile_padding_small tile_borders_bottom main__row">
+        <b-player-buttons />
       </b-tile>
     </div>
   </div>
@@ -12,9 +21,10 @@
 import { Component, Vue } from 'nuxt-property-decorator';
 import BPlayer from '~/components/player/player.vue';
 import BTile from '~/components/tile/tile.vue';
+import BPlayerButtons from '~/components/player-buttons/player-buttons.vue';
 
 @Component({
-  components: { BTile, BPlayer },
+  components: { BPlayerButtons, BTile, BPlayer },
 })
 export default class MainPage extends Vue {}
 </script>
