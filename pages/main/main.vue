50e0f01b4ec6ea2a04fe01b19a247e7905d40ce3
intelrug <intelrug@gmail.com>
2020-08-29 01:05:00 +0400

    feat: implement announcements functionality

--- pages/main/main.vue
+++ pages/main/main.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="main">
     <div class="main__player">
-      <b-tile class="tile_padding_small tile_borders_top main__row">
-        <a href="#" class="main__announcement">
-          <span>27.08.2020</span> в <span>20:30</span> <sup>UTC+3</sup> –
-          Гостевая рубрика с Midipon
+      <b-tile
+        v-if="event"
+        class="tile_padding_small tile_borders_top main__row"
+      >
+        <a v-if="isLive" href="#" class="main__announcement">
+          <span>В эфире</span>
+          – {{ summary }}
+        </a>
+        <a v-else href="#" class="main__announcement">
+          <span>{{ date }}</span> в <span>{{ time }}</span>
+          <i v-if="lessThan10Hours">
+            (через {{ remainingTime | toRemainingTime }})
+          </i>
+          – {{ summary }}
         </a>
       </b-tile>
-      <b-tile class="tile_padding_medium main__row">
+      <b-tile
+        class="tile_padding_medium main__row"
+        :class="[{ tile_borders_top: !event }]"
+      >
         <b-player />
       </b-tile>
       <b-tile class="tile_padding_small tile_borders_bottom main__row">
@@ -19,14 +32,55 @@
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator';
+import moment from 'moment';
 import BPlayer from '~/components/player/player.vue';
 import BTile from '~/components/tile/tile.vue';
 import BPlayerButtons from '~/components/player-buttons/player-buttons.vue';
+import { CalendarEvent } from '~/graphql/schema';
+import { toRemainingTime } from '~/tools/filters';
 
 @Component({
   components: { BPlayerButtons, BTile, BPlayer },
+  filters: { toRemainingTime },
 })
-export default class MainPage extends Vue {}
+export default class MainPage extends Vue {
+  get events(): CalendarEvent[] {
+    return this.$accessor.player.calendarEventsState || [];
+  }
+
+  get event(): CalendarEvent | null {
+    return this.events[0];
+  }
+
+  get lessThan10Hours(): boolean {
+    return this.remainingTime < 10 * 60 * 60;
+  }
+
+  get date(): string {
+    return moment(this.event?.startsAt || 0).format('DD.MM.YYYY');
+  }
+
+  get time(): string {
+    return moment(this.event?.startsAt || 0).format('HH:mm');
+  }
+
+  get remainingTime(): number {
+    let time = (this.event?.startsAt || 0) - this.$accessor.now;
+    if (time < 0) time = 0;
+    return time / 1000;
+  }
+
+  get isLive() {
+    return (
+      this.remainingTime === 0 &&
+      this.$accessor.player.track.type === 'livestream'
+    );
+  }
+
+  get summary(): string {
+    return this.event?.summary || '';
+  }
+}
 </script>
 
 <style lang="stylus" src="./main.styl"></style>
--- tools/filters.ts
+++ tools/filters.ts
@@ -1,16 +1,23 @@
-export function toHHMMSS(time: string | number) {
+function getHoursMinutesSeconds(time: string | number) {
   const secs = parseFloat(String(time));
   const hours: number = Math.floor(secs / (60 * 60));
-  let minutes: number | string = Math.floor((secs - hours * 60 * 60) / 60);
-  let seconds: number | string = Math.floor(
+  const minutes: number | string = Math.floor((secs - hours * 60 * 60) / 60);
+  const seconds: number | string = Math.floor(
     secs - minutes * 60 - hours * 60 * 60,
   );
 
-  if (seconds < 10) {
-    seconds = `0${seconds}`;
-  }
-  if (hours && minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  return `${hours ? `${hours}:` : ''}${minutes}:${seconds}`;
+  return { seconds, minutes, hours };
+}
+
+export function toHHMMSS(time: string | number) {
+  const { seconds, minutes, hours } = getHoursMinutesSeconds(time);
+  const secondsString = `${seconds}`.padStart(2, '0');
+  const minutesString = `${minutes}`.padStart(2, '0');
+  return `${hours ? `${hours}:` : ''}${minutesString}:${secondsString}`.trim();
+}
+
+export function toRemainingTime(time: string | number) {
+  const { minutes, hours } = getHoursMinutesSeconds(time);
+  const minutesString = `${minutes}`.padStart(2, '0');
+  return `${hours ? `${hours} ч.` : ''} ${minutesString} м.`.trim();
 }
