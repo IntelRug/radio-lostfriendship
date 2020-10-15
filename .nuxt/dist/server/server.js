3963ebeb9e689cdc4b60fdc9f6113a761e28bc21
intelrug <intelrug@gmail.com>
2020-10-15 20:46:20 +0400

    Updates

--- .nuxt/dist/server/server.js
+++ .nuxt/dist/server/server.js
@@ -7504,7 +7504,7 @@ async function createApp(ssrContext, config = {}) {
       "link": [{
         "rel": "icon",
         "type": "image\u002Fpng",
-        "href": "\u002Flogo.png"
+        "href": "\u002Ffavicon.png"
       }, {
         "rel": "stylesheet",
         "href": "https:\u002F\u002Ffonts.googleapis.com\u002Fcss2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap"
